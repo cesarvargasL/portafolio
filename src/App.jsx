70082ffff0c039c03905b3b37e79f5d2e939d6eb
César L Vargas Lezano <cesarluisvargaslezano@gmail.com>
2023-06-28 05:49:02 +0000

    Commit two

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "César Luis Vargas Lezano",
+  title: "Developer and student of enginer systems",
+  email: "cesarvargaslezano@outlook.com",
+  gitHub: "cesarvargasL",
+  instagram: "htx_cxsar",
+  linkedIn: "César Luis Vargas Lezano",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
